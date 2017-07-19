cac6a95ae50621a24cf3c8c6bfe3644089993b06
Lassier Antoine <toinousp@gmail.com>
2017-07-19 18:32:40 +0200

    ✨ Ajout du footer et ajustement configs

---
+++ components/footer.vue
@@ -0,0 +1,48 @@
+<template>
+    <footer class="footer">
+        <p class="footer__item">© 2017 Antoine Lassier.</p>
+        <p class="footer__item footer__item--grayed">
+            <a href="">antoine.lassier@gmail.com</a>
+        </p>
+        <p class="footer__item footer__item--grayed">Merci d'être passé ❤️</p>
+    </footer>
+</template>
+
+<style lang="scss">
+@import '~assets/scss/_variables.scss';
+
+/* Footer
+   ========================================================================== */
+
+/**
+ * 1. Taille du texte réduite dans le footer.
+ * 2. Réduit le padding sur téléphone, pour afficher plus.
+ */
+.footer {
+    padding: 2.5em .5em;
+    text-align: center;
+    font-size: 14px; /* 1 */
+    font-weight: normal;
+    @include respond-to($phone) {
+        padding: 1em .1em; /* 2 */
+    }
+}
+
+.footer__item--grayed {
+    color: $soft-grey;
+}
+
+/**
+ * 1. Les paragraphes s'affichent sur la même ligne.
+ * 2. Espace horizontal entre les items.
+ * 3. Réduit l'espace horizontal et ajoute une espace vertial sur téléphone.
+ */
+ 
+.footer__item {
+    display: inline-block; /* 1 */
+    margin: 0 1em; /* 2 */
+    @include respond-to($phone) {
+        margin: .25em .5em; /* 3 */
+    }
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,49 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'starter',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** Run ESLINT on save
+    ** Headers of the page
     */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    head: {
+        title: 'starter',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+        ],
+        script: [
+            { src: 'https://use.typekit.net/txv3mea.js' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    css: [
+        'normalize.css',
+        { src: '~assets/scss/main.scss', lang: 'scss' }
+    ],
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#3B8070' },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** Run ESLINT on save
+        */
+        extend(config, ctx) {
+            /* const cssLoader = config.module.rules.find((loader) => loader.test.toString() === '/\\.css$/')
+            //cssLoader.use.push('postcss-loader')
+            cssLoader.use.splice(2, 0, 'postcss-loader'); */
+
+            if (ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
