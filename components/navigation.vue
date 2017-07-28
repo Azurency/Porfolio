8e31d4480183bf4c199718d7ea5d38cafb44bd46
Lassier Antoine <toinousp@gmail.com>
2017-07-28 17:23:19 +0200

    ✨ Animation des liens de navigation

--- components/navigation.vue
+++ components/navigation.vue
@@ -40,19 +40,60 @@ export default {
         display: inline-block; /* 1 */
         margin: 0 .8em;
         font-size: 14px;
+        position: relative;
     }
 
     &__link {
         color: inherit;
         transition: color $default-transition-duration;
+        display: inline-block;
+        padding: $container-outside-gutter/4 0;
 
         &:hover {
             color: $dark-text;
+        }
+
+        &::before {
+            content: '';
+            position: absolute;
+            display: block;
+            bottom: 0;
+            left: 0;
+            height: 1px;
+            width: 0%;
+            transition: width $default-transition-duration ease;
+        }
+
+        &::after {
+            content: '';
+            position: absolute;
+            display: block;
+            bottom: 0;
+            right: 0;
+            height: 1px;
+            width: 0%;
+            border-bottom: $hover-border-width solid;
+            transition: width $longer-transition-duration ease;
+        }
+
+        &:hover::before {
+            width: 100%;
+            border-bottom: $hover-border-width solid;
+        }
+
+        &:hover::after {
+            width: 100%;
+            border-bottom: $hover-border-width solid transparent;
+            transition: all 0s ease;
         }
     }
 
     &--colored {
         color: $fountain-blue;
+
+        & .nav__link:hover {
+            color: $fountain-blue-hover;
+        }
     }
 }
 </style>
